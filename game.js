a70495ab9488ca6f3394a16ad85ce70812ee959b
Sean Tant <seantant@umich.edu>
2025-03-03 16:33:05 -0500

    Write getComputerChoice() function and begin getHumanChoice()

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+function getComputerChoice() {
+    randInt = Math.floor(Math.random() * 3);
+
+    if (randInt === 0) {
+        return "rock";
+    }
+    else if (randInt === 1) {
+        return "paper";
+    }
+    return "scissors";
+}
+
+function getHumanChoice() {
+    choice = prompt("Please enter your choice (rock, paper, scissors)").toLowerCase;
+
+    
+}
+
+console.log(getComputerChoice())
